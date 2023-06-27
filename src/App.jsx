3f546f31d25c3627fca2bf6c63ccc7b10783ed9c
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-27 08:36:58 -0700

    configure basic component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-export const App = () => {
-  return null;
-};
+import { Grid, GridItem } from '@chakra-ui/react';
+import { Header, Main, Footer } from './components';
 
-export default App;
+export const App = () => {
+  const Layout = () => (
+    <Grid
+      templateAreas={`"header"
+                  "main"
+                  "footer"`}
+      gridTemplateRows={'50px 1fr 30px'}
+      gridTemplateColumns={'1fr'}
+      minH='100vh'
+      color='blackAlpha.700'
+      fontWeight='bold'>
+      <GridItem pl='2' bg='orange.300' area={'header'}>
+        <Header />
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        <Main />
+      </GridItem>
+      <GridItem pl='2' bg='blue.300' area={'footer'}>
+        <Footer />
+      </GridItem>
+    </Grid>
+  );
+
+  return <Layout />;
+};
