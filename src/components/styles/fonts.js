def014790b210fba5b2cbc403a671c237db6ac66
Nic Mortelliti <npmortelliti@gmail.com>
2023-06-28 11:52:17 -0700

    add fonts

---
+++ src/components/styles/fonts.js
@@ -0,0 +1,4 @@
+export const fonts = {
+  heading: `'Satisfy', sans-serif`,
+  body: `'Mulish Variable', sans-serif`,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './components/styles/theme';
+import '@fontsource/satisfy/400.css';
+import '@fontsource-variable/mulish';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
